6a361407077ec3094cd4bb7d0c6a8ceaa2682b9c
Le Thi My Hang <hang.my.le2003@gmail.com>
2023-02-01 15:43:15 +0700

    Web login

---
+++ scritp.js
@@ -0,0 +1,20 @@
+// // console.log(". ");
+
+// secret = Math.floor(Math.random() * 101);
+
+
+// let input = prompt("Input secretnumber you guess: ");
+// let inputnmber = parseInt(input);
+// while(true){
+//         if(inputnmber == secret)
+//     {
+//         alert("you are right");
+//         console.log(input);
+//     }
+//     else
+//     {
+//         alert("You wrong");
+//         console.log(input);
+//     }
+
+// }
